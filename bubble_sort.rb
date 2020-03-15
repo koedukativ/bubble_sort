5561c3fc0d82067455279e12fb22204208a5c7a9
Joschka César <cesar@posteo.de>
2020-03-15 18:59:20 +0100

    Implemented Bubble_Sort Method

---
+++ bubble_sort.rb
@@ -0,0 +1,12 @@
+def bubble_sort(array)
+    iteration = 1
+    while iteration < array.length
+      count = 0
+        while count+iteration < array.length
+            array[count+1], array[count] = array[count], array[count+1] if array[count] > array[count+1]
+            count +=1
+        end
+      iteration +=1
+    end
+    puts array
+  end
