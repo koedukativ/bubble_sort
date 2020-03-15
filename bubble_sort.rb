efb5593cce7dad799a13bfa634f2f1319f724ff4
Joschka César <cesar@posteo.de>
2020-03-15 19:12:39 +0100

    Implemented Sort_by Method

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -10,3 +10,20 @@ def bubble_sort(array)
     end
     puts array
   end
+
+def bubble_sort_by(array)
+    iteration = 1
+    while iteration < array.length
+      count = 0
+        while count+iteration < array.length
+            array[count+1], array[count] = array[count], array[count+1] if yield(array[count], array[count+1]) > 0
+            count +=1
+        end
+      iteration +=1
+    end
+    puts array
+  end
+
+bubble_sort_by(["hi","hello","hey"]) do |left,right|
+  left.length - right.length
+end
